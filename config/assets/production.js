7da3b30de4a13d73d4c292192d0ac73dc8477746
davidbulnes <davidbulnes19@hotmail.com>
2018-05-30 18:50:20 -0500

    cambio

--- config/assets/production.js
+++ config/assets/production.js
@@ -7,16 +7,25 @@ module.exports = {
     lib: {
       css: [
         // bower:css
+        'public/lib/angular-material/angular-material.min.css',
         'public/lib/angular-ui-notification/dist/angular-ui-notification.min.css',
         'public/lib/bootstrap/dist/css/bootstrap.min.css',
         'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+        'public/lib/angular-bootstrap-calendar/dist/css/angular-bootstrap-calendar.min.css',
+        'public/lib/angular-bootstrap-colorpicker/css/colorpicker.min.css'
         // endbower
       ],
       js: [
         // bower:js
         'public/lib/angular/angular.min.js',
+        'public/lib/jquery/dist/jquery.min.js',
+        'public/lib/moment/min/moment-with-locales.min.js',
+        'public/lib/angular-bootstrap-calendar/dist/js/angular-bootstrap-calendar-tpls.js',  
         'public/lib/angular-animate/angular-animate.min.js',
+        'public/lib/angular-aria/angular-aria.min.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
+        'public/lib/bootstrap/dist/js/bootstrap.min.js',
+        'public/lib/angular-bootstrap-colorpicker/js/bootstrap-colorpicker-module.js',
         'public/lib/angular-messages/angular-messages.min.js',
         'public/lib/angular-mocks/angular-mocks.js',
         'public/lib/angular-resource/angular-resource.min.js',
@@ -24,6 +33,14 @@ module.exports = {
         'public/lib/angular-ui-router/release/angular-ui-router.min.js',
         'public/lib/ng-file-upload/ng-file-upload.min.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/interactjs/dist/interactjs.min.js',
+        'public/lib/chart.js/dist/Chart.min.js',
+        'public/lib/angular-chart.js/dist/angular-chart.min.js',
+        'public/lib/konva/konva.min.js',
+        'public/lib/angular-pintura/angular-pintura.min.js',
+        'public/lib/angular-material/angular-material.min.js',
+        'public/lib/angular-data-grid/dist/pagination.min.js',
+        'public/lib/angular-data-grid/dist/dataGrid.min.js'
         // endbower
       ]
     },
