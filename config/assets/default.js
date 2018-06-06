69550ddb1412121428e3bb93a79836b6d5de167b
davidbulnes <davidbulnes19@hotmail.com>
2018-06-06 01:05:38 -0500

    test

--- config/assets/default.js
+++ config/assets/default.js
@@ -24,7 +24,7 @@ module.exports = {
         'public/lib/angular-bootstrap-calendar/dist/js/angular-bootstrap-calendar-tpls.js',  
         'public/lib/angular-animate/angular-animate.js',
         //'public/lib/angular-aria/angular-aria.js',
-        'node-modules/angular-aria/angular-aria.js',
+       // 'modules/core/client/js/angular-aria/angular-aria.js',
         'public/lib/angular-touch/angular-touch.min.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
         'public/lib/bootstrap/dist/js/bootstrap.min.js',
@@ -42,7 +42,7 @@ module.exports = {
         'public/lib/konva/konva.js',
         'public/lib/angular-pintura/angular-pintura.js',
         //'public/lib/angular-material/angular-material.js',
-        'node-modules/angular-material/angular-material.js',
+        //'modules/core/client/js/angular-material/angular-material.js',
         'public/lib/angular-data-grid/dist/pagination.js',
         'public/lib/angular-data-grid/dist/dataGrid.js'
 
